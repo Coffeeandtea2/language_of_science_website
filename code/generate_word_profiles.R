0a651ff8f3206c06d8c13f1622325fa564b5b569
George Moroz <agricolamz@gmail.com>
2023-08-16 23:48:31 +0300

    update

--- code/generate_word_profiles.R
+++ code/generate_word_profiles.R
@@ -6,63 +6,63 @@ library(readxl)
 
 # extract paradigms -------------------------------------------------------
 
-read_xlsx("data/word_profiles.xlsx") |>
-  filter(!is.na(lemma_for_site)) |>  
-  distinct(lemma_for_site) |> 
-  mutate(lemma_for_site = str_split(lemma_for_site, " - ")) |> 
-  unnest_longer(lemma_for_site) |> 
-  mutate(lemma_for_site = str_remove_all(lemma_for_site, "\\(.*?\\)")) |> 
-  pull(lemma_for_site) |> 
-  walk(function(i){
-    read_html(glue("https://ru.wiktionary.org/wiki/{i}")) |>
-      html_element(".morfotable") |>
-      write_lines(glue("data/{i}.html"))
-
-    read_lines(glue("data/{i}.html")) |>
-      str_remove("float:right; ") |>
-      str_remove_all('<a href.*?>') |>
-      str_remove_all('</a>') |>
-      append("<details>", after = 0) |>
-      append("<summary>парадигма</summary>", after = 1) |>
-      append("</details>") |>
-      write_lines(glue("data/{i}.html"))
-  })
-
-# merge 2 paradimes of verbs with different aspects
-
-read_xlsx("data/word_profiles.xlsx") |>
-  filter(str_detect(lemma_for_site, "-")) |>
-  mutate(lemma = str_extract(lemma_for_site, "^.*?(?=( -))"),
-         lemma = ifelse(is.na(lemma), lemma_for_site, lemma),
-         lemma = str_remove_all(lemma, "\\(.*?\\)")) |> 
-  distinct(lemma, lemma_for_site) |> 
-  mutate(lemma_for_site = str_split(lemma_for_site, " - ")) |> 
-  unnest_longer(lemma_for_site) |> 
-  group_by(lemma) |> 
-  reframe(n = 1:n(),
-          n = str_c("variant_", n),
-          lemma_for_site = lemma_for_site) |> 
-  mutate(lemma_for_site = str_remove_all(lemma_for_site, "\\(.*?\\)")) |> 
-  pivot_wider(names_from = n, values_from = lemma_for_site) ->
-  merge_paradigms
-
-walk(merge_paradigms$lemma, function(i){
-  v1 <- merge_paradigms[merge_paradigms$lemma == i,]$variant_1
-  v2 <- merge_paradigms[merge_paradigms$lemma == i,]$variant_2
-  
-  read_lines(str_c("data/", v2, ".html")) |> 
-    str_remove_all("<details>") |> 
-    str_remove_all("<summary>парадигма</summary>") ->
-    v2_modified
-  
-  read_lines(str_c("data/", v1, ".html")) |> 
-    str_remove_all("</details>") |> 
-    append(v2_modified) ->
-    result
-  
-  write_lines(result, str_c("data/", i, ".html"))
-  
-})
+# read_xlsx("data/word_profiles.xlsx") |>
+#   filter(!is.na(lemma_for_site)) |>  
+#   distinct(lemma_for_site) |> 
+#   mutate(lemma_for_site = str_split(lemma_for_site, " - ")) |> 
+#   unnest_longer(lemma_for_site) |> 
+#   mutate(lemma_for_site = str_remove_all(lemma_for_site, "\\(.*?\\)")) |> 
+#   pull(lemma_for_site) |> 
+#   walk(function(i){
+#     read_html(glue("https://ru.wiktionary.org/wiki/{i}")) |>
+#       html_element(".morfotable") |>
+#       write_lines(glue("data/{i}.html"))
+# 
+#     read_lines(glue("data/{i}.html")) |>
+#       str_remove("float:right; ") |>
+#       str_remove_all('<a href.*?>') |>
+#       str_remove_all('</a>') |>
+#       append("<details>", after = 0) |>
+#       append("<summary>парадигма</summary>", after = 1) |>
+#       append("</details>") |>
+#       write_lines(glue("data/{i}.html"))
+#   })
+# 
+# # merge 2 paradimes of verbs with different aspects
+# 
+# read_xlsx("data/word_profiles.xlsx") |>
+#   filter(str_detect(lemma_for_site, "-")) |>
+#   mutate(lemma = str_extract(lemma_for_site, "^.*?(?=( -))"),
+#          lemma = ifelse(is.na(lemma), lemma_for_site, lemma),
+#          lemma = str_remove_all(lemma, "\\(.*?\\)")) |> 
+#   distinct(lemma, lemma_for_site) |> 
+#   mutate(lemma_for_site = str_split(lemma_for_site, " - ")) |> 
+#   unnest_longer(lemma_for_site) |> 
+#   group_by(lemma) |> 
+#   reframe(n = 1:n(),
+#           n = str_c("variant_", n),
+#           lemma_for_site = lemma_for_site) |> 
+#   mutate(lemma_for_site = str_remove_all(lemma_for_site, "\\(.*?\\)")) |> 
+#   pivot_wider(names_from = n, values_from = lemma_for_site) ->
+#   merge_paradigms
+# 
+# walk(merge_paradigms$lemma, function(i){
+#   v1 <- merge_paradigms[merge_paradigms$lemma == i,]$variant_1
+#   v2 <- merge_paradigms[merge_paradigms$lemma == i,]$variant_2
+#   
+#   read_lines(str_c("data/", v2, ".html")) |> 
+#     str_remove_all("<details>") |> 
+#     str_remove_all("<summary>парадигма</summary>") ->
+#     v2_modified
+#   
+#   read_lines(str_c("data/", v1, ".html")) |> 
+#     str_remove_all("</details>") |> 
+#     append(v2_modified) ->
+#     result
+#   
+#   write_lines(result, str_c("data/", i, ".html"))
+#   
+# })
 
 
 # generate tasks ----------------------------------------------------------
@@ -108,9 +108,12 @@ walk(unique(generate_tasks$lemma), function(i){
 
 ```{{r}}
 #| results: asis
-check_question(answer = '{answer}', 
-               right = 'все верно!', 
-               wrong = 'к сожалению нет, попробуйте еще раз...')
+checkdown::check_question(answer = '{answer}', 
+                          right = 'все верно!', 
+                          wrong = 'к сожалению нет, попробуйте еще раз...',
+                          button_label = 'проверить')
+checkdown::check_hint(hint_text = '{answer}',
+                      hint_title = 'Нажмите, чтобы посмотреть ответ')
 ```
 
 ") }) |> 
@@ -152,7 +155,10 @@ map2(generate_declension_tasks$task,
 #| results: asis
 checkdown::check_question(answer = '{answer}', 
                           right = 'все верно!', 
-                          wrong = 'к сожалению нет, попробуйте еще раз...')
+                          wrong = 'к сожалению нет, попробуйте еще раз...',
+                          button_label = 'проверить')
+checkdown::check_hint(hint_text = '{answer}',
+           hint_title = 'Нажмите, чтобы посмотреть ответ')                          
 ```
 
 ") }) |> 
@@ -175,7 +181,10 @@ map2(generate_government_tasks$task,
 #| results: asis
 checkdown::check_question(answer = '{answer}', 
                           right = 'все верно!', 
-                          wrong = 'к сожалению нет, попробуйте еще раз...')
+                          wrong = 'к сожалению нет, попробуйте еще раз...',
+                          button_label = 'проверить')
+checkdown::check_hint(hint_text = '{answer}',
+           hint_title = 'Нажмите, чтобы посмотреть ответ')
 ```
 
 ") }) |> 
